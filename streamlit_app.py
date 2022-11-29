a6bad3bc8ceae0883faf9ae554b8bda67b933313
Kuldeep Sonawane <sonawane.kuldeep@gmail.com>
2022-11-29 15:35:06 +0530

    Update streamlit_app.py
    initial Header and title added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit 
 streamlit.title('My Parent New Healty Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Bluebeery Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie ')
+streamlit.text('Hard-Boiled Free-Range Egg')
