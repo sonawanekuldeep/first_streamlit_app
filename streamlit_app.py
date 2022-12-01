1d8dce43a6b742bb848cf9396502e3eed6de6306
Kuldeep Sonawane <sonawane.kuldeep@gmail.com>
2022-12-01 21:55:06 +0530

    Update streamlit_app.py
    updating data

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,15 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select current_user(), current_account(), current_regirm()")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from snowflake:")
-streamlit.text("my_data_row")
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
+
+
+
+
 
 
