44c941ca7874b13d52c6f661028400b9663ad1ce
Kuldeep Sonawane <sonawane.kuldeep@gmail.com>
2022-12-01 23:14:40 +0530

    Update streamlit_app.py
    code orgnized

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruitvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+    #take the jons version of the response and normalize it
+    fruitvice_normalized = pd.json_normalize(fruityvice_response.json())
+    #output it the screen as a table
+    streamlit.dataframe(fruitvice_normalized)
+    
+execpt URLError as e:
+  streamlit.error()
+    
 streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the jons version of the response and normalize it
-fruitvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruitvice_normalized)
 
 streamlit.stop()
 
@@ -45,7 +52,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
 
 add_my_fruit = streamlit.text_input('What Fruit would you like to add?', 'Jackfruit')
 streamlit.write('Thanks for adding ' + add_my_fruit)
